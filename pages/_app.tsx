77f6110f4ca97621a0b61edc8d2ca9903ae40ef6
LeeCherry731 <sangevol0@gmail.com>
2021-09-11 17:59:54 +0700

    add material UI and test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
+        <title>NextStock</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
---
+++ readme.txt
@@ -0,0 +1,11 @@
+// with npm
+npm install @material-ui/core
+
+// with yarn
+yarn add @material-ui/core
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+
+yarn add @material-ui/icons
